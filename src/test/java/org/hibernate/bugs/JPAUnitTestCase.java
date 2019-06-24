bc15a76035e9c6195cb6b3a768964af87d0b8920
Dmitry YUshakov <d.yushakov@rogii.com>
2019-06-24 08:51:51 +0400

    better test case

--- src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -1,13 +1,17 @@
 package org.hibernate.bugs;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 import org.hibernate.bugs.entity.DescendantEntity;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using the Java Persistence API.
@@ -29,7 +33,7 @@ public class JPAUnitTestCase {
 	// Entities are auto-discovered, so just add them anywhere on class-path
 	// Add your tests, using standard JUnit.
 	@Test
-	public void hhh123Test() throws Exception {
+	public void joinedEntityPersisted() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
 
@@ -42,5 +46,10 @@ public class JPAUnitTestCase {
 
 		entityManager.getTransaction().commit();
 		entityManager.close();
+
+		EntityManager secondManager = entityManagerFactory.createEntityManager();
+		List resultList = secondManager.createNativeQuery("select * from descendant").getResultList();
+		assertThat(resultList.size(), equalTo(1));
+		secondManager.close();
 	}
 }
